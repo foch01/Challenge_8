edfdcf068188e71fc0a82dfadda95c85ac1edc0a
foch01 <tsanceo@yahoo.com>
2016-07-26 15:13:31 +0200

    ajout compteur

--- js_quizz-master/quizz.js
+++ js_quizz-master/quizz.js
@@ -3,12 +3,15 @@ $(".answerFalse").click(faux);
 $("#slider-prev").click(previous);
 $("#slider-next").click(next);
 
+var count = 1;
+
 function validate (){
     $(this).addClass("green");
 }
 
 function faux () {
     $(this).addClass("faux");
+    
 }
 
 var slider = $('.bxslider').bxSlider({
@@ -16,11 +19,23 @@ var slider = $('.bxslider').bxSlider({
 });
 
 function previous (){
+
+    if (count > 1){;
+    count -=1;
     slider.goToPrevSlide();
+     }
+    var x = document.getElementsByClassName("count");
+    x[0].innerHTML = "Question : " + count + "/10";
 }
 
 function next (){
+    
+    if (count < 10){;
+    count +=1;
     slider.goToNextSlide();
+     }
+    var x = document.getElementsByClassName("count");
+    x[0].innerHTML = "Question : " + count + "/10";
 }
 
 
